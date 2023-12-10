46c58e3d3cb488b31898b7d30068bf3e1391992e
rhythamitaliya <ritaliya2003@gmail.com>
2023-12-10 21:17:09 +0530

    10-12-2023

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const { Op } = require('sequelize');
 const cors = require('cors');
+const uuid = require('uuid');
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
@@ -13,22 +14,28 @@ app.use(cookieParser('QWERTYUIOPLKJHGFDSAZXCVBNM'));
 
 //  RIGISTER API
 app.post('/register', (req, res) => {
+
     const { users } = require('./models');
+
     var createUser = {}
     try {
+
         if (req.body.password.length < 8) {
             return res.status(200).send('Password length should be more than 8 characters...');
         }
+
         createUser = users.create(req.body);
         if (!createUser) {
             return res.status(400).send({ 'error': 'Could not register. Please retry later...' });
         };
+
         res.status(201).send({ 'success': true });
+
         console.log(createUser);
     } catch (e) {
         return res.status(200).send(e.message);
     }
-    
+
 });
 
 // LOGIN API
@@ -61,6 +68,41 @@ app.post('/login', async (req, res) => {
         return res.status(201).send({ "X-Access-Token": token, });
     } else {
         return res.status(401).send({ 'error': 'username and password is incorrect....' });
+    }
+});
+
+
+// P A S S W O R D - R E S E T
+
+app.post('/reset/password/:token', async (req, res) => {
+
+    const { users } = require('./models');
+    
+    const userP = await users.findOne({
+        where: {
+            token: req.params.token
+        }
+    });
+
+    if (userP == null) {
+        return res.send('The link is invalid...');
+    }
+
+    if (req.body.password1 == req.body.password2) {
+        users.update({
+            password: bcrypt.hashSync(req.body.password2, 10),
+            token: uuid.v1(),
+            isActive: 1
+        }, {
+            where: {
+                id: userP.id
+            }
+        });
+
+        res.send('Your password has been successfully updated....');
+
+    } else {
+        res.send('Passwords do not match');
     }
 });
 
